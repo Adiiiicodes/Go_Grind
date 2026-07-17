aedd4c79c4d6e7275d22fbf5a0274d9df7b3e38f
Adiiiicodes <nalawadeaditya017@gmail.com>
2026-07-17 21:12:54 +0530

    notes added and main file cleaned

--- Mergesort/main.go
+++ Mergesort/main.go
@@ -7,31 +7,3 @@ func main() {
 	sortedArray := algo.Mergesort(arr)
 	fmt.Println("Sorted Array is --> ", sortedArray)
 }
-
-/*func mergesort(arr []int) []int {
-	if len(arr) <= 1 {
-		return arr
-	}
-	mid := len(arr)/2
-	left := mergesort(arr[:mid])
-	right := mergesort(arr[mid:])
-	return merge(left, right)
-}
-
-func merge (left , right []int) []int {
-	i , j := 0 , 0 
-	result := []int{}
-	for i< len(left) && j<len(right) {
-		if left[i]< right[j] {
-			result = append(result, left[i])
-			i++
-		} else {
-			result = append(result , right[j])
-			j++
-		}
-	}
-
-	result = append(result, left[i:]...)
-	result = append(result, right[j:]...)
-	return result
-}*/
